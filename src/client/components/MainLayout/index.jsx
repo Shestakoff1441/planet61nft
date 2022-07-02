4c7045a84f2b17dbe65e3e5083c0a426f3e1f9d5
Shestakoff1441 <shashashestakoff@gmail.com>
2022-07-02 12:46:30 +0200

    added title

--- src/client/components/MainLayout/index.jsx
+++ src/client/components/MainLayout/index.jsx
@@ -1,13 +1,17 @@
-
-import Header from '../Header';
-import FirstSection from '../FirstSection';
-import SecondSection from '../SecondSection';
-import ThirdSection from '../ThirdSection';
-import styles from './MainLayout.module.scss';
+import Header from '../Header'
+import FirstSection from '../FirstSection'
+import SecondSection from '../SecondSection'
+import ThirdSection from '../ThirdSection'
+import styles from './MainLayout.module.scss'
+import Head from 'next/head'
 
 const MainLayout = () => {
   return (
     <main className={styles.main}>
+      <Head>
+        <title>Planet 61</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <FirstSection />
       <SecondSection />
