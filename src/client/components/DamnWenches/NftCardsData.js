6e9da671159d731a966b9c612ce892b3717cc69f
Shestakoff1441 <shashashestakoff@gmail.com>
2022-07-09 18:26:43 +0200

    fixes

--- src/client/components/DamnWenches/NftCardsData.js
+++ src/client/components/DamnWenches/NftCardsData.js
@@ -1,20 +1,22 @@
 import Teya from "../../assets/images/damnWenches/1.svg";
-import Lily from "../../assets/images/damnWenches/lil.jpeg";
+import hacker from "../../assets/images/damnWenches/hacker.svg";
 export const cartsData = [
   {
     name: "Teya",
     text:
       "Having climbed the Olympus of underground street racing, she failed to curb the need for speed, causing irreparable damage to some of the largest cities of the world. Keep your eyes peeled, no one knows which track will be next...",
     image: Teya,
-    href: "https://www.binance.com/fr/nft/profile/planet61-3949254e9009569f223e41a9c530aef2",
+    href:
+      "https://www.binance.com/fr/nft/profile/planet61-3949254e9009569f223e41a9c530aef2",
     buttonText: "VIEW ON MARKETPLACE"
   },
   {
-    name: "Lily",
+    name: "Trinity",
     text:
-      "Lily is a happy young wife. She is fond of fashion and baking. In her wardrobe, there are clothes of famous brands. As for baking: what special ingredient does she add to pies, given that at 26 she is a twice millionaire widow?",
-    image: Lily,
-    href: "https://www.binance.com/fr/nft/profile/planet61-3949254e9009569f223e41a9c530aef2",
+      " A childhood spent in the computer workshop of an introverted father. Soulless monitors played a key role in her asociality and gave birth to a passion for technology, making Trinity an expert in software. The collapse of the world economy or the launch of nuclear missiles is only a matter of her imagination",
+    image: hacker,
+    href:
+      "https://www.binance.com/fr/nft/profile/planet61-3949254e9009569f223e41a9c530aef2",
     buttonText: "VIEW ON MARKETPLACE"
   }
 ];
