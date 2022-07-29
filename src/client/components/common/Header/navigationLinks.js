41512d05053533720807194bcee8a1fc3471b4a8
Shestakoff1441 <shashashestakoff@gmail.com>
2022-07-29 15:11:08 +0200

    test

--- src/client/components/common/Header/navigationLinks.js
+++ src/client/components/common/Header/navigationLinks.js
@@ -1,12 +1,6 @@
 export const navigationLinks = [
-  { title: "1000 AMC", href: "#amc", isLink: true },
+  { title: "ANDROMEDA", href: "#amc", isLink: true },
   { title: "ROADMAP", href: "#rm", isLink: true },
-  {
-    title: "COLLECTIONS",
-    isLink: false,
-    subTitles: [
-      { title: "LOST IN UNIVERSE", href: "/liu" },
-      { title: "DAMN WENCHES", href: "/dm" }
-    ]
-  }
+  { title: "MINDMAP", href: "#mm", isLink: true },
+
 ];
