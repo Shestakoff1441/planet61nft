bf44d3bdd9d831d1fe235a7c61810a314f8acdd6
Shestakoff1441 <shashashestakoff@gmail.com>
2022-08-03 16:32:17 +0200

    zaf

--- src/client/components/RoadMap/index.jsx
+++ src/client/components/RoadMap/index.jsx
@@ -1,11 +1,56 @@
 import Section from "../common/Section";
 import styles from "./Roadmap.module.scss";
 
+import frida from "../../assets/images/frida.svg";
+import classnames from "classnames";
+
+const WithRightBorder = () => (
+  <div className={classnames(styles.rightBorderBlock)}>
+    <div className={styles.stageTitles}>
+      <div className={styles.stageNumber}>Stage1</div>
+      <div className={styles.stageName}>Mint</div>
+    </div>
+    <ul className={styles.goalsList}>
+      <li>1</li>
+      <li>2</li>
+    </ul>
+  </div>
+);
+
+const WithLeftBorder = () => (
+  <div className={classnames(styles.leftBorderBlock)}>
+    <div className={styles.stageTitles}>
+      <div className={styles.stageNumber}>Stage1</div>
+      <div className={styles.stageName}>Mint</div>
+    </div>
+    <ul className={styles.goalsList}>
+      <li>1</li>
+      <li>2</li>
+    </ul>
+  </div>
+);
+
+const EmptyBlock = () => <div className={styles.emtyBlock}>hello</div>;
+
+const data = [
+  { title: "hey", type: "right" },
+  { title: "" },
+  { title: "" },
+  { title: "hey", type: "left" }
+];
+
+const RoadmapBlock = {
+  right: WithRightBorder,
+  left: WithLeftBorder
+};
 const Roadmap = () => {
   return (
-    <Section title="ROADMAP">
+    <Section title="ROADMAP" background={frida}>
       <div className={styles.roadmapContainer}>
-
+        {data.map((el, index) => {
+          const RoadmapComponent = RoadmapBlock[el.type];
+          return el.title ? <RoadmapComponent /> : <EmptyBlock />;
+        })}
       </div>
     </Section>
   );
--- src/client/components/common/Section/index.jsx
+++ src/client/components/common/Section/index.jsx
@@ -1,26 +1,13 @@
 import classes from "classnames";
 import styles from "./Section.module.scss";
 
-const getHeader = (content, size) => {
-  return [
-    <h1>{content}</h1>,
-    <h2>{content}</h2>,
-    <h3>{content}</h3>,
-    <h4>{content}</h4>,
-    <h5>{content}</h5>,
-    <h6>{content}</h6>
-  ][size];
-};
-
 const Section = ({
   background: { src } = "",
   title = "",
-  titleSize = 2,
   // bgc,
   children,
   customStyles
 }) => {
-  const text = getHeader(title, titleSize);
 
   return (
     <section
@@ -31,7 +18,7 @@ const Section = ({
       }}
     >
       <div className={styles.sectionContainer}>
-        <div className={styles.sectionTitle}>{title ? text : null}</div>
+        <div className={styles.sectionTitle}>{title}</div>
         {children}
       </div>
     </section>
