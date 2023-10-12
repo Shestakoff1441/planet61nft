f2e5beabfae35750b9bd58af89da80d9dcde05d8
Aliaksandr Shastakou <aliaksandr.shastakou@ingenios.io>
2023-10-12 15:52:13 +0200

    break tests

--- __tests__/minus/index.test.js
+++ __tests__/minus/index.test.js
@@ -4,7 +4,7 @@ describe("minus functions", () => {
   it("should return result after minus function", () => {
     // check if all components are rendered
     expect(minus(5, 3)).toBe(2);
-    expect(minus(5, 5)).toBe(0);
+    expect(minus(5, 5)).toBe(1);
     expect(minus(-5, 1)).toBe(-6);
   });
 });
