387983ce9f85aad1d8d80438f5e4616128e4ead6
Aliaksandr Shastakou <aliaksandr.shastakou@ingenios.io>
2024-01-08 18:24:07 +0100

    still works

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,4 @@
-import ReactGA from "react-ga4";
-
-
 import "../styles/globals.scss";
-ReactGA.initialize("G-R6GH2KD393");
-ReactGA.send({ hitType: "pageview", page: "/", title: "Main Title" });
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
